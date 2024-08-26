1e3205ed948e95b809842281c960163c4a30ff12
Brion Blais <brion.blais@outlook.com>
2024-08-26 12:55:35 -0400

    Created a new branch to include the first name in the program and changed comments.

--- Chapter2Examples.cpp
+++ Chapter2Examples.cpp
@@ -5,11 +5,12 @@ Programmer : Brion Blais
 Date : 08 / 2024
 Requirements:
 Write a program that has the following character variables:
-first, middle, and last. Store your intitials in these variables
+first, middle,last, and firstName. Store your intitials and first name in these variables
 then display them on the screen.
 */
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
@@ -17,12 +18,16 @@ int main()
     char firstInitial;
     char middleInitial;
     char lastInitial;
+    string firstName;
     cout << "Please enter the initial of your first name:";
     cin >> firstInitial;
     cout << "Please enter the initial of your middle name:";
     cin >> middleInitial;
     cout << "Please enter the initial of your last name:";
     cin >> lastInitial;
-    cout << "Your initals are: " << firstInitial << middleInitial << lastInitial;
+    cout << "Your initals are: " << firstInitial << middleInitial << lastInitial << endl;
+    cout << "Please enter your first name:";
+    cin >> firstName;
+    cout << "Your first name is : " << firstName;
     return 0;
 }
